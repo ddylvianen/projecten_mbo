4128b6a35594aa81cd213e4dda6b9d57462608ad
Dylan Vianen <dylchelvia@gmail.com>
2024-12-04 22:59:11 +0100

    changed urls.py

--- game_site/urls.py
+++ game_site/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('game.urls')),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
