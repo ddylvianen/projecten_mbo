5c4795f5350ad7cd131d485e93ff88a645e1a818
Dylan Vianen <dylchelvia@gmail.com>
2025-01-22 21:16:10 +0100

    made the site responsive and fixed minor bugs

--- static/js/home.js
+++ static/js/home.js
@@ -1,10 +1,18 @@
 const dropdown = document.getElementById('dropdown');
+const logout = document.getElementById('logout');
 const button = document.getElementById('button');
 const messages = document.querySelectorAll('.messages li');
 
 
 button.addEventListener('click', () => {
   dropdown.classList.toggle('show');
+  try {
+    logout.classList.toggle('hide');
+  }
+  catch {
+    console.log('no logout');
+  }
+  // logout.classList.toggle('hide');
   console.log('click');
 });
 
